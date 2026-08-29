b4b480a8c27858f5dc9e90e2d13a71ab2532acab
MartinChicaiza <m.chicaiza@uniandes.edu.co>
2026-08-28 20:52:01 -0500

    Add files via upload

---
+++ EstacionEspacial/algorithms/search.py
@@ -0,0 +1,74 @@
+from algorithms.problems import SearchProblem
+import algorithms.utils as utils
+from world.game import Directions
+from algorithms.heuristics import nullHeuristic
+
+
+def tinyDiagnosticSearch(problem: SearchProblem):
+    """
+    Returns a hard-coded sequence of moves for the tinyDiagnostic layout.
+    For any other station layout, the sequence of moves will be incorrect.
+    """
+    s = Directions.SOUTH
+    e = Directions.EAST
+    return [s, e, s, e, e, e, e, s, e, e, s, s, e, s, s, e, s, e, e, e, e, e, e, e]
+
+
+def depthFirstSearch(problem: SearchProblem):
+    """
+    Search the deepest nodes in the search tree first.
+
+    Your search algorithm needs to return a list of actions that reaches the
+    goal. Make sure to implement a graph search algorithm.
+    """
+
+    frontera = utils.Stack()
+    visitados = set()
+
+    frontera.push((problem.getStartState(), []))
+
+    while not frontera.isEmpty():
+        estado, camino = frontera.pop()
+
+        if problem.isGoalState(estado):
+            return camino
+
+        if estado not in visitados:
+            visitados.add(estado)
+            for sucesor, accion, _ in problem.getSuccessors(estado):
+                if sucesor not in visitados:
+                    frontera.push((sucesor, camino + [accion]))
+
+    return []
+
+
+def breadthFirstSearch(problem: SearchProblem):
+    """
+    Search the shallowest nodes in the search tree first.
+    """
+    # TODO: Add your code here
+    utils.raiseNotDefined()
+
+
+def uniformCostSearch(problem: SearchProblem):
+    """
+    Search the node of least total cost first.
+    """
+
+    # TODO: Add your code here
+    utils.raiseNotDefined()
+
+
+def aStarSearch(problem: SearchProblem, heuristic=nullHeuristic):
+    """
+    Search the node that has the lowest combined cost and heuristic first.
+    """
+    # TODO: Add your code here
+    utils.raiseNotDefined()
+
+
+# Abbreviations (you can use them for the -f option in main.py)
+bfs = breadthFirstSearch
+dfs = depthFirstSearch
+astar = aStarSearch
+ucs = uniformCostSearch
